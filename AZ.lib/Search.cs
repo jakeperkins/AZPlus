580cf5f6f320f8b6e71b8ecddb93b1e938c28097
Jake Perkins <jake.perkinsr@live.com>
2014-09-17 16:46:34 -0500

    Finished Discount story.

--- AZ.lib/Search.cs
+++ AZ.lib/Search.cs
@@ -11,7 +11,28 @@ namespace AZ.lib
 	{
 		public IList<Book> GetBooks(User user)
 		{
-			throw new System.NotImplementedException();
+			foreach (var userBook in user.OwnedBooks)
+			{
+				foreach (var amazonBook in AmazonBooks)
+				{
+					if (userBook.IsSameTitleAndAuthor(amazonBook) && userBook.BookType == BookType.Kindle &&
+					    amazonBook.BookType == BookType.Paperback)
+					{
+						amazonBook.DiscountPercentage = Discount;
+					}
+				}
+			}
+
+			return AmazonBooks;
 		}
+
+		internal IList<Book> AmazonBooks = new[] 
+		{
+			new Book{Author = "author1", Title = "book1", BookType = BookType.Hardback},
+			new Book{Author = "author2", Title = "book2", BookType = BookType.Paperback},
+			new Book{Author = "author3", Title = "book3", BookType = BookType.Kindle}
+		};
+
+		private const int Discount = 10;
 	}
 }
