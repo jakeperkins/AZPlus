907b45b91952c1d21fdceab1792a20f2512982cc
Jake Perkins <jake.perkinsr@live.com>
2014-09-17 16:16:03 -0500

    Adding Search.cs

---
+++ AZ.lib/Search.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace AZ.lib
+{
+	public interface ISearch
+	{
+		IList<Book> GetBooks(User user);
+	}
+
+	public class Search : ISearch
+	{
+		public IList<Book> GetBooks(User user)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
