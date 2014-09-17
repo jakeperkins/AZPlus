870f90a2690640648950497142f6e5e515ef6ead
mattheweagin <matthew_eagin@dell.com>
2014-09-17 16:32:57 -0500

    Adding User and User.BuysBook method

---
+++ AZ.lib.UnitTests/UserTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+
+namespace AZ.lib.UnitTests
+{
+    [TestClass]
+    public class UserTests
+    {
+        private User _user;
+        private IList<Book> _books;
+
+        [TestMethod]
+        public void UserHasAListOfOwnedBooks()
+        {
+            GivenANewUser();
+            ThenTheListOfOwnedBooksHasSize(0);
+        }
+
+        [TestMethod]
+        public void CanAddABookToOwnedBooks()
+        {
+            GivenANewUser();
+            WhenABookIsPurchased();
+            ThenTheListOfOwnedBooksHasSize(1);
+        }
+
+        private void WhenABookIsPurchased()
+        {
+            _user.BuysBook(new Book
+            {
+                Author = "John Doe",
+                BookType = BookType.Kindle,
+                DiscountPercentage = 0,
+                Title = "Testing Purchases"
+            });
+        }
+
+        private void GivenANewUser()
+        {
+            _user = new User();
+        }
+
+        private void ThenTheListOfOwnedBooksHasSize(int bookCount)
+        {
+            _user.OwnedBooks.Count.ShouldEqual(bookCount);
+        }
+    }
+}
--- AZ.lib/User/User.cs
+++ AZ.lib/User/User.cs
@@ -1,7 +1,21 @@
-﻿namespace AZ.lib
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AZ.lib
 {
 	public class User
 	{
-		 
+		public User ()
+		{
+			OwnedBooks = new List<Book>();
+		}
+
+		public IList<Book> OwnedBooks { get; set; }
+
+		public void BuysBook(Book book)
+		{
+			OwnedBooks.Add(book);
+		}
 	}
 }
