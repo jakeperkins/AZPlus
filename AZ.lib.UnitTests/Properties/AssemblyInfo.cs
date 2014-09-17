bea44e757acd3c3f92041d4a30020cb687df96df
Jake Perkins <jake.perkinsr@live.com>
2014-09-17 09:51:53 -0500

    Adding Unit Tests Project

--- AZ.lib.UnitTests/Properties/AssemblyInfo.cs
+++ AZ.lib.UnitTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AZ.lib")]
+[assembly: AssemblyTitle("AZ.lib.UnitTests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("AZ.lib")]
+[assembly: AssemblyProduct("AZ.lib.UnitTests")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f9a520aa-08c6-47fe-8f17-94fcccaacffb")]
+[assembly: Guid("bb1b7955-1938-44ac-8822-36e420c2714d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,6 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: InternalsVisibleTo("AZPlus.lib.UnitTests")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
