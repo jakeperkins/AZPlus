e82c234b16c7b2ace345c2b25cb980e31b83d28d
mattheweagin <matthew_eagin@dell.com>
2014-09-17 10:08:25 -0500

    UnitTest changes

---
+++ AZ.lib.UnitTests/WishListTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Should;
+
+namespace AZ.lib.UnitTests
+{
+    [TestClass]
+    public class WishListTests
+    {
+        [TestMethod]
+        public void WishListCanHoldItems()
+        {
+            var wishList = new WishList();
+            wishList.Items.ShouldNotBeNull();
+        }
+
+        [TestMethod]
+        public void WishListIsEmpty()
+        {
+            var wishList = new WishList();
+            wishList.Items.Count.ShouldEqual(0);
+        }
+    }
+}
---
+++ AZ.lib/WishList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AZ.lib
+{
+    public class WishList
+    { 
+        public WishList()
+        {
+            Items = new List<object>();
+        }
+
+        public IList<object> Items { get; set; }
+    }
+}
